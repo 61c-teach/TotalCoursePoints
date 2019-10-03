3a40251a2b092ef1c68643ac60b65e730a1b32d5
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-10-03 10:33:37 -0700

    Forgot id

--- student.py
+++ student.py
@@ -68,7 +68,7 @@ class Student:
         cur_score = self.total_points()
         cur_max_score = c.get_total_possible(only_inputted=True)
         b = c.grade_bins.relative_bin(cur_score, cur_max_score)
-        return f"You are on track for a(n) {b}"
+        return f"You are on track for a(n) {b.id}"
 
     def apply_extensions(self):
         for ext_cat_key, value in self.extensionData.items():
