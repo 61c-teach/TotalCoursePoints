cf784243ce2ab89380bceece3905371a5f0ee261
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-12-16 10:12:56 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md") as f:
     readme = f.read()
 
 setup(name='TotalCoursePoints',
-      version='0.1',
-      description="This is the backend which is meant to make generating student grades easier.",
+      version='1.0.0',
+      description="This is the backend which makes generating student grades easier.",
       long_description=readme,
       author='Stephan Kaminsky',
       author_email='skaminsky115@berkeley.edu',
